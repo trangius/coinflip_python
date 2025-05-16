d579fcb23fd8b05e27fe0440f4def44d712b45ea
Krister Trangius <krister@trangius.se>
2025-05-16 08:26:43 +0200

    Lade till felhantering för ogiltig inmatning

--- coinflip.py
+++ coinflip.py
@@ -3,7 +3,15 @@
 
 import random
 
-count = int(input("Hur många gånger vill du singla slant? "))
+try:
+    count = int(input("Hur många gånger vill du singla slant? "))
+    if count < 1:
+        print("Felaktigt antal.")
+        exit()
+except ValueError:
+    print("Du måste skriva ett heltal.")
+    exit()
+
 for _ in range(count):
     flip = random.randint(0, 1)
     if flip == 0:
