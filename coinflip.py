8958bed9a97bf67d005bc1557446b2089492f791
Krister Trangius <krister@trangius.se>
2025-05-16 08:26:07 +0200

    Lade till inmatning för antal kast

--- coinflip.py
+++ coinflip.py
@@ -3,8 +3,10 @@
 
 import random
 
-flip = random.randint(0, 1)
-if flip == 0:
-    print("Krona")
-else:
-    print("Klave")
+count = int(input("Hur många gånger vill du singla slant? "))
+for _ in range(count):
+    flip = random.randint(0, 1)
+    if flip == 0:
+        print("Krona")
+    else:
+        print("Klave")
